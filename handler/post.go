8b9d83cb42bd81c0c8bc71c538f03b55cf0d9194
joshuabl97 <blau.joshua@gmail.com>
2023-08-19 11:47:19 -0400

    fixed POST /shortcut to conform to new data packagae functions

--- handler/post.go
+++ handler/post.go
@@ -32,7 +32,7 @@ func (h *HandlerHelper) CreateShortcut(w http.ResponseWriter, r *http.Request) {
 		Msg("Processing AddEndpoint")
 
 	var err error
-	h.db, err = data.AddEndpoint(h.l, h.db, request.Endpoint, request.URL)
+	h.db, err = data.AddEndpoint(h.db, request.Endpoint, request.URL)
 	if err != nil {
 		h.l.Error().Err(err).Msg("Failed to add request body to DB")
 		http.Error(w, "Failed to add request body to DB", http.StatusInternalServerError)
